02c4f96b53d6b4acbbba2afb73e2c4c7512fc0e4
Ben <bjeffrey92@hotmail.com>
2023-03-24 08:59:26 +0000

    plotting function

--- plot1.py
+++ plot1.py
@@ -9,7 +9,7 @@ from scipy.signal import argrelextrema
 N = 15
 
 
-def smooth_function(x: Union[int, float, np.array]) -> Union[int, float, np.array]:
+def smooth_function(x: Union[int, float, np.ndarray]) -> Union[int, float, np.ndarray]:
     return (x**3 - 10 * x**2 + 100 + 0.001 * x**4) / 2
 
 
@@ -40,11 +40,16 @@ def plot1_data(
         return xfit, yfit, yfit_with_noise
 
 
-def compute_envelopes(y_with_noise: np.array) -> Tuple[np.array]:
+def get_extrema(y_with_noise: np.ndarray) -> Tuple[np.ndarray]:
     idx_local_maxima = argrelextrema(y_with_noise, np.greater)
     idx_local_minima = argrelextrema(y_with_noise, np.less)
+    return idx_local_maxima, idx_local_minima
 
-    def envelope(idx_extrema: np.array) -> np.array:
+
+def compute_envelopes(
+    y_with_noise: np.ndarray, idx_local_maxima: np.ndarray, idx_local_minima: np.ndarray
+) -> Tuple[np.array]:
+    def envelope(idx_extrema: np.ndarray) -> np.ndarray:
         extrema = y_with_noise[idx_extrema]
 
         x = idx_extrema[0]
@@ -56,8 +61,44 @@ def compute_envelopes(y_with_noise: np.array) -> Tuple[np.array]:
     return *envelope(idx_local_maxima), *envelope(idx_local_minima)
 
 
+def plot(
+    x: np.ndarray,
+    y: np.ndarray,
+    y_with_noise: np.ndarray,
+    upper_envelope: np.ndarray,
+    lower_envelope: np.ndarray,
+    idx_local_maxima: np.ndarray,
+    idx_local_minima: np.ndarray,
+    local_maxima: np.ndarray,
+    local_minima: np.ndarray,
+):
+    plt.clf()
+    plt.plot(x[20:], y[20:], color="black", linestyle="--", label="Signal")
+    plt.plot(x[20:], y_with_noise[20:], color="blue", label="Noise")
+    plt.plot(
+        x[20:], upper_envelope[21:-1], color="green", label="Upper & Lower Envelopes"
+    )
+    plt.plot(x[20:], lower_envelope[21:-1], color="green")
+    plt.scatter(x[idx_local_maxima], local_maxima, label="Local Maxima", color="red")
+    plt.scatter(x[idx_local_minima], local_minima, label="Local Minima", color="orange")
+    plt.legend()
+    plt.show()
+
+
 def main():
     x, y, y_with_noise = plot1_data(smooth_function, sin_amplitude=200, sin_exponent=5)
+    idx_local_maxima, idx_local_minima = get_extrema(y_with_noise)
     local_maxima, upper_envelope, local_minima, lower_envelope = compute_envelopes(
-        y_with_noise
+        y_with_noise, idx_local_maxima, idx_local_minima
+    )
+    plot(
+        x,
+        y,
+        y_with_noise,
+        upper_envelope,
+        lower_envelope,
+        idx_local_maxima,
+        idx_local_minima,
+        local_maxima,
+        local_minima,
     )
